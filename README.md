8dae78d697a0ec9fa203677fb5b3fae001095413
Marcos Neves <marcos.neves@gmail.com>
2024-06-14 10:56:51 -0300

    v0.2.1 fix missing exports for decorators

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # @cf-worker/utils
+
+## TODO
+
+- [ ] Make sure that all functions are exported by mod.ts
--- mod.ts
+++ mod.ts
@@ -1,4 +1,5 @@
 export * from "./cors/mod.ts"
+export * from "./decorators/mod.ts"
 export * from "./responses/mod.ts"
 export * from "./routers/mod.ts"
 export type * from "./types.d.ts"
