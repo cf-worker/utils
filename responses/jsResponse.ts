d466478f829ea4cc654ab522184f45d68ea8aacf
Marcos Neves <marcos.neves@gmail.com>
2024-08-08 00:12:19 -0300

    test jsResponse

--- responses/jsResponse.ts
+++ responses/jsResponse.ts
@@ -1,5 +1,12 @@
 import { contentTypeResponse } from "./contentTypeResponse.ts"
 
+/**
+ * Creates a JavaScript response.
+ *
+ * @param {BodyInit | Response} body - The body of the response.
+ * @param {ResponseInit} [init] - Optional additional options for the response.
+ * @return {Response} The JavaScript response.
+ */
 export function jsResponse(body: BodyInit | Response, init?: ResponseInit): Response {
   return contentTypeResponse(body, "application/javascript", init)
 }
