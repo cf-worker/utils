c7e116dcc2a5b58ca8cef6c25d2b27c195deb4da
Marcos Neves <marcos.neves@gmail.com>
2024-08-09 11:36:27 -0300

    swithRouter new params

--- routers/switchRouter.test.ts
+++ routers/switchRouter.test.ts
@@ -1,20 +1,6 @@
+import { expect } from "bun:test"
 import { assertEquals } from "@std/assert"
 import { switchRouter } from "./switchRouter.ts"
-
-Deno.test("switchRouter GET success", () => {
-  const method = "GET"
-  const url = "https://example.com/users/123"
-
-  const router = switchRouter({ method, url })
-
-  assertEquals(router.GET("/users/:id"), "GET /users/123")
-  assertEquals(router.params, { id: "123" })
-
-  assertEquals(router.POST("/users/:id"), undefined)
-  assertEquals(router.PUT("/users/:id"), undefined)
-  assertEquals(router.PATCH("/users/:id"), undefined)
-  assertEquals(router.DELETE("/users/:id"), undefined)
-})
 
 function route(method: string, url: string) {
   const r = switchRouter({ method, url })
@@ -37,6 +23,21 @@ function route(method: string, url: string) {
   }
 }
 
+Deno.test("switchRouter GET success", () => {
+  const method = "GET"
+  const url = "https://example.com/users/123"
+
+  const router = switchRouter({ method, url })
+
+  assertEquals(router.GET("/users/:id"), "GET /users/123")
+  assertEquals(router.params, { id: "123" })
+
+  assertEquals(router.POST("/users/:id"), undefined)
+  assertEquals(router.PUT("/users/:id"), undefined)
+  assertEquals(router.PATCH("/users/:id"), undefined)
+  assertEquals(router.DELETE("/users/:id"), undefined)
+})
+
 Deno.test("switchRouter router", () => {
   assertEquals(route("GET", "https://example.com/users"), undefined)
   assertEquals(route("GET", "https://example.com/users/1"), "1")
@@ -44,4 +45,56 @@ Deno.test("switchRouter router", () => {
   assertEquals(route("PUT", "https://example.com/users/3"), "3")
   assertEquals(route("PATCH", "https://example.com/users/4"), "4")
   assertEquals(route("DELETE", "https://example.com/users/5"), "5")
+})
+
+Deno.test("switchRouter initial params", () => {
+  const method = "GET"
+  const url = "https://example.com/users/123"
+  const KV = 456
+  const { params, GET } = switchRouter({ method: method, url: url }, { KV })
+  GET("/users/:id")
+  const kv = params.KV
+  expect(kv).toBe(456)
+  expect(params.KV).toBe(KV)
+  expect(params.id).toBe("123")
+  expect(params.foo).toBeUndefined()
+})
+
+Deno.test("switchRouter route params don't override initial params", () => {
+  const method = "GET"
+  const url = "https://example.com/users/123"
+  const id = 456
+  const { params, GET } = switchRouter({ method: method, url: url }, { id })
+  GET("/users/:id")
+  expect(params.id).toBe(id)
+})
+
+Deno.test("switchRouter route default params", () => {
+  const method = "GET"
+  const url = "https://example.com/users/123"
+  const page = "1"
+  const { params, GET } = switchRouter({ method: method, url: url }, undefined, { page })
+  GET("/users/:id")
+  expect(params.id).toBe("123")
+  expect(params.page).toBe(page)
+})
+
+Deno.test("switchRouter route default params can be overrided", () => {
+  const method = "GET"
+  const url = "https://example.com/users/123/page/2"
+  const page = "1"
+  const { params, GET } = switchRouter({ method: method, url: url }, undefined, { page })
+  GET("/users/:id/page/:page")
+  expect(params.id).toBe("123")
+  expect(params.page).toBe("2")
+})
+
+Deno.test("switchRouter params are the same", () => {
+  const method = "GET"
+  const url = "https://example.com/users/123/page/2"
+  const page = "1"
+  const iniParams = { page }
+  const { params, GET } = switchRouter({ method: method, url: url }, iniParams)
+  GET("/users/:id/page/:page")
+  expect(params).toBe(iniParams)
 })
--- routers/switchRouter.ts
+++ routers/switchRouter.ts
@@ -1,9 +1,13 @@
 import type { Dict, MethodUrl } from "../types.ts"
 import { matchUrl } from "./matchUrl.ts"
 
-type Return = {
+type LooseObject<T extends Record<string, unknown>> = T & {
+  [key: string]: string | undefined
+}
+
+type Return<P extends Record<string, unknown>> = {
   methodPath: string
-  params: Dict
+  params: LooseObject<P>
   method: string
   path: string
   ANY: (pattern: string) => string | undefined
@@ -14,21 +18,26 @@ type Return = {
   DELETE: (pattern: string) => string | undefined
 }
 
-export function switchRouter({ method, url }: MethodUrl, params: Dict = {}): Return {
+export function switchRouter<Fixed extends Record<string, unknown>, Default extends Dict>(
+  { method, url }: MethodUrl,
+  params: Fixed = {} as Fixed,
+  defaultParams?: Default,
+): Return<Fixed & Default> {
   const path = new URL(url).pathname
   const methodPath = `${method} ${path}`
   const noop = (_: string) => undefined
+  const fixedParams = Object.assign({}, params)
   function ANY(pattern: string) {
     const args = matchUrl(pattern, url)
     if (args) {
-      Object.assign(params, args, params)
+      Object.assign(params, defaultParams, args, fixedParams)
       return methodPath
     }
   }
 
   return {
     methodPath,
-    params,
+    params: params as LooseObject<Fixed & Default>,
     method,
     path,
     ANY,
