a2737fcd08fb09bc09056989aa8e1bb5585921b1
Marcos Neves <marcos.neves@gmail.com>
2024-08-19 00:00:03 -0300

    docs: :bulb:

--- encoding/base64Encode.ts
+++ encoding/base64Encode.ts
@@ -3,6 +3,7 @@
  *
  * @param {ArrayLike<number> | ArrayBufferLike} buffer - The buffer to be encoded.
  * @return {string} The base64 encoded string.
+ * @see https://tc39.es/proposal-arraybuffer-base64/
  */
 export function base64Encode(buffer: ArrayLike<number> | ArrayBufferLike): string {
   return btoa(
