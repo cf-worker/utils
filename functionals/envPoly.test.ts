53131ced67529e188b66f9904a1a106178976a82
Marcos Neves <neves64@icloud.com>
2025-09-30 22:09:03 -0300

    100% test coverage

--- functionals/envPoly.test.ts
+++ functionals/envPoly.test.ts
@@ -5,3 +5,35 @@ test("envPoly", () => {
   expect(envPoly("DOES_NOT_EXISTS")).toBeUndefined()
   expect(typeof envPoly("PWD")).toBe("string")
 })
+
+test("Bun", () => {
+  if ("Bun" in globalThis) return
+  const Deno = globalThis.Deno
+  // @ts-expect-error: Deno may not exist on globalThis
+  delete globalThis.Deno
+  // deno-lint-ignore no-explicit-any
+  const global: any = globalThis
+  global.Bun = {
+    env: {
+      HOME: "/home/root",
+    },
+  }
+  expect(envPoly("HOME")).toBe(global.Bun.env.HOME)
+  globalThis.Deno = Deno
+})
+
+test("process", () => {
+  if ("process" in globalThis) return
+  const Deno = globalThis.Deno
+  // @ts-expect-error: Deno may not exist on globalThis
+  delete globalThis.Deno
+  // deno-lint-ignore no-explicit-any
+  const global: any = globalThis
+  global.process = {
+    env: {
+      HOME: "/home/root",
+    },
+  }
+  expect(envPoly("HOME")).toBe(global.process.env.HOME)
+  globalThis.Deno = Deno
+})
