3f02300f7fad39eeb9f2e12d8402f3f0809dcbb9
Marcos Neves <neves64@icloud.com>
2026-03-10 10:26:31 -0300

    Fix deno task test

--- mod.test.ts
+++ mod.test.ts
@@ -9,8 +9,9 @@ const paths = (await Array.fromAsync(
     maxLevel: 2,
     recursive: true,
     filterFile: ({ name, parentPath }) =>
-      name.endsWith(".ts") && !name.endsWith(".deno.ts") && !name.endsWith(".test.ts") &&
-      !name.endsWith("mod.ts") && !name.endsWith("types.ts") && !parentPath.includes("/lab/"),
+      name.endsWith(".ts") && !name.endsWith(".d.ts") && !name.endsWith(".deno.ts") &&
+      !name.endsWith(".test.ts") && !name.endsWith("mod.ts") && !name.endsWith("types.ts") &&
+      !parentPath.includes("/lab/") && !parentPath.includes("/node_modules/"),
   }),
 )).map((entry) => entry.path)
 
